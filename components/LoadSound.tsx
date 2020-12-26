737c60f2071457022e25fc17ff0a941c0a238b4a
jurelik <jure@likar.me>
2020-12-26 12:39:06 +0000

    check file size client-side before uploading

--- components/LoadSound.tsx
+++ components/LoadSound.tsx
@@ -3,6 +3,10 @@ import { changeSoundSend } from 'redux/actions';
 
 const LoadSound = (props: any) => {
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files[0].size > 3000000) {
+      console.log('File size must be below 2MB.');
+      return;
+    }
     props.changeSoundSend(props.trackId, e.target.files[0]);
   }
 
