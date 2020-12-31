537065e8bb4b8626ca5dc00e2da00605654f19bc
jurelik <jure@likar.me>
2020-12-31 12:02:52 +0000

    updated ws protocol to wss

--- utils/setupSocket.ts
+++ utils/setupSocket.ts
@@ -1,6 +1,6 @@
 const setupSocket = (id: string | string[]) => {
   return new Promise<WebSocket>((resolve) => {
-    const socket = id ? new WebSocket(`ws://${process.env.NEXT_PUBLIC_CONNECTION_URL}/${id}`) : new WebSocket(`ws://${process.env.NEXT_PUBLIC_CONNECTION_URL}`);
+    const socket = id ? new WebSocket(`${process.env.NODE_ENV === 'production' ? 'wss' : 'ws'}://${process.env.NEXT_PUBLIC_CONNECTION_URL}/${id}`) : new WebSocket(`${process.env.NODE_ENV === 'production' ? 'wss' : 'ws'}://${process.env.NEXT_PUBLIC_CONNECTION_URL}`);
 
     socket.onopen = () => {
       console.log('Connected to server.');
