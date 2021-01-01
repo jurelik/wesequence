90850762d0e3990fa8db5a842d8781f250b53dff
jurelik <jure@likar.me>
2021-01-01 23:47:04 +0000

    started implementing add & join room functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const handleCreateRoom = () => {
+
+  }
+
+  const handleJoinRoom = () => {
+
+  }
+
   return (
     <div className={styles.container}>
+      <button>Create a Room</button>
+      <p>or</p>
+      <div style={{ display: 'flex' }}>
+        <input />
+        <button>Join a Room</button>
+      </div>
     </div>
   )
 }
