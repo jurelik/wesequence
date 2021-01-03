f29925ecabac8cf38be5705708be99dae0a15583
jurelik <jure@likar.me>
2021-01-03 18:59:13 +0000

    successfully implemented creating a new room

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
+import { useRouter } from 'next/router';
 import styles from '../styles/Home.module.css';
 import {} from 'redux/actions';
 
 const Home = () => {
-  const handleCreateRoom = () => {
+  const router = useRouter();
 
+  const handleCreateRoom = async () => {
+    try {
+      const _res = await fetch(`${process.env.NODE_ENV === 'production' ? 'https' : 'http'}://${process.env.NEXT_PUBLIC_CONNECTION_URL}/api/create`);
+      const res = await _res.json();
+
+      if (res.type === 'SUCCESS') {
+        router.push(`/${res.name}`);
+      }
+      else {
+        throw res.err;
+      }
+    }
+    catch (err) {
+      console.log(err);
+    }
   }
 
   const handleJoinRoom = () => {
@@ -12,7 +28,7 @@ const Home = () => {
 
   return (
     <div className={styles.container}>
-      <button>Create a Room</button>
+      <button onClick={handleCreateRoom}>Create a Room</button>
       <p>or</p>
       <div style={{ display: 'flex' }}>
         <input />
