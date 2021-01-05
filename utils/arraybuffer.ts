aacfeaa09688b995f762668fc8ca0c1cda93673f
jurelik <jure@likar.me>
2021-01-05 19:46:22 +0000

    installed base64-arraybuffer npm module to deal with encoding/decoding

--- utils/arraybuffer.ts
+++ utils/arraybuffer.ts
@@ -1,27 +1,9 @@
-// Taken from: https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
-export const arraybufferToString = (buf: ArrayBuffer) => {
-  var bufView = new Uint16Array(buf);
-  var length = bufView.length;
-  var result = '';
-  var addition = Math.pow(2,16)-1;
+import { encode, decode } from 'base64-arraybuffer';
 
-  for(var i = 0; i<length; i+=addition){
-    if(i + addition > length){
-        addition = length - i;
-    }
-    result += String.fromCharCode.apply(null, bufView.subarray(i,i+addition));
-  }
-
-  return result;
+export const arraybufferToString = (buffer: ArrayBuffer) => {
+  return encode(buffer);
 }
 
-export const stringToArraybuffer = (str: string) => {
-  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-  var bufView = new Uint16Array(buf);
-
-  for (var i=0, strLen=str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-
-  return buf;
+export const stringToArraybuffer = (string: string) => {
+  return decode(buffer);
 }
